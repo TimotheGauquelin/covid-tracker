c27de338c8b00375387223be7beb8a155c93f9ee
T <gauquelintimothe@gmail.com>
2020-08-14 15:06:18 +0200

    Update Data according to Country

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -11,16 +11,32 @@ export class CountriesComponent implements OnInit {
 
   data: GlobalDataSummary[];
   countries : string[] = [];
+  totalActive: number;
+  totalConfirmed: number;
+  totalDeaths: number;
+  totalRecovered: number;
 
   constructor(private service : DataServiceService) { }
 
-  ngOnInit(): void {
-
+  ngOnInit(){
     this.service.getGlobalData().subscribe(result => {
       this.data = result;
       this.data.forEach(cs => {
         this.countries.push(cs.country)
       })
-    })}
+    })
+  }
+
+  updateValues(country : string) {
+    console.log(country);
+    this.data.forEach( cs => {
+      if(cs.country == country) {
+        this.totalActive = cs.active
+        this.totalConfirmed = cs.confirmed
+        this.totalDeaths = cs.deaths
+        this.totalRecovered = cs.recovered
+      }
+    })
+  }
 
 }
